668072834bd8aaf514e010eedc7d7051c530b50b
orta <git@orta.io>
2021-06-07 16:23:15 +0000

    Deploying to gh-pages from @ shikijs/twoslash@544694df8729dadc76a949f649e09ff631f3f84f 🚀

---
+++ _next/static/Yzp3hp55q5kCTKrCSJjuk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-5bda5a1c058b35f5239e.js"],"/_error":["static/chunks/pages/_error-b30902e13465df7c5366.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
