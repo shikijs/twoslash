b791c6e431061b24f5bc27e7eeaea5f01b37b3fe
Frenco <hey@frenco.dev>
2021-06-18 12:04:11 +0630

    refactor(cli): update example renders

--- packages/twoslash-cli/examples/codefence.ts
+++ packages/twoslash-cli/examples/codefence.ts
@@ -1,8 +1,7 @@
 // twoslash: { theme: "dark-plus" }
 // codefence: {1}
 function greet(person: string, date: Date) {
-    console.log(`Hello ${person}, today is ${date.toDateString()}!`)
-  }
-  
-  greet("Maddison", new Date())
-  
+  console.log(`Hello ${person}, today is ${date.toDateString()}!`)
+}
+
+greet("Maddison", new Date())
