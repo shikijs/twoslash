c145bb3469b46aeb01ae2ca800395a42ad23cb0f
Orta <git@orta.io>
2021-06-25 13:59:21 +0100

    Make the versions script do pnpm i also

--- scripts/syncVersions.js
+++ scripts/syncVersions.js
@@ -2,40 +2,44 @@
 const glob = require("glob")
 const { readFileSync, writeFileSync } = require("fs")
 const { format } = require("prettier")
+const { execSync } = require("child_process")
 
-// Make sure that all the versioning is accurate across the packages
-const pgkPaths = glob.sync("packages/*/package.json")
-const packages = pgkPaths.map(p => ({ path: p, pkg: JSON.parse(readFileSync(p, "utf8")) }))
-const inWorkspace = dep => {
-  return packages.find(p => p.pkg.name === dep)
-}
-
-packages.forEach(d => {
-  const p = d.pkg
-  const deps = [p.devDependencies || {}, p.dependencies || {}]
-  let write = false
-  deps.forEach(d => {
-    const keysInWorkSpace = Object.keys(d).filter(dep => inWorkspace(dep))
-    keysInWorkSpace.forEach(key => {
-      const version = packages.find(p => p.pkg.name === key).pkg.version
-      if (d[key] !== version) {
-        console.error(`${p.name} has the wrong dependency for: ${key}. Expected ${version} got ${d[key]}`)
-        process.exitCode = 1
-        d[key] = version
-        write = true
-      }
-    })
-  })
-
-  if (write) {
-    const [major, minor, patch] = p.version.split(".")
-    if (major !== "0" && minor !== "0" && patch !== "0") {
-      p.version = `${major}.${minor}.${Number(patch) + 1}`
-    }
-    writeFileSync(d.path, format(JSON.stringify(p), { filepath: d.path }))
+const doVersions = () => {
+  // Make sure that all the versioning is accurate across the packages
+  const pgkPaths = glob.sync("packages/*/package.json")
+  const packages = pgkPaths.map(p => ({ path: p, pkg: JSON.parse(readFileSync(p, "utf8")) }))
+  const inWorkspace = dep => {
+    return packages.find(p => p.pkg.name === dep)
   }
-})
-
-if (process.exitCode) {
-  console.log("You can automatically fix these fails by running `node scripts/syncVersions.js`")
+  
+  packages.forEach(d => {
+    const p = d.pkg
+    const deps = [p.devDependencies || {}, p.dependencies || {}]
+    let write = false
+    deps.forEach(d => {
+      const keysInWorkSpace = Object.keys(d).filter(dep => inWorkspace(dep))
+      keysInWorkSpace.forEach(key => {
+        const version = packages.find(p => p.pkg.name === key).pkg.version
+        if (d[key] !== version) {
+          console.error(`${p.name} has the wrong dependency for: ${key}. Expected ${version} got ${d[key]}`)
+          process.exitCode = 1
+          d[key] = version
+          write = true
+        }
+      })
+    })
+  
+    if (write) {
+      const [major, minor, patch] = p.version.split(".")
+      if (major !== "0" && minor !== "0" && patch !== "0") {
+        p.version = `${major}.${minor}.${Number(patch) + 1}`
+      }
+      writeFileSync(d.path, format(JSON.stringify(p), { filepath: d.path }))
+    }
+  })
 }
+
+doVersions()
+doVersions()
+
+execSync("pnpm i", { stdio: "pipe" })
