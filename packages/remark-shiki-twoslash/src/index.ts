c3915c66c02de4d3e71fdd43adc5b1ec1ae23d78
Orta <git@orta.io>
2021-06-26 15:07:11 +0100

    Use a weakmap for the highlighters

--- packages/remark-shiki-twoslash/src/index.ts
+++ packages/remark-shiki-twoslash/src/index.ts
@@ -59,29 +59,27 @@ export const runTwoSlashOnNode = (code: string, lang: string, meta: string, sett
 }
 
 // To make sure we only have one highlighter per theme in a process
-const highlighterCache = new Map<IThemeRegistration, Highlighter>()
+const highlighterCache = new WeakMap<UserConfigSettings, Highlighter[]>()
 
 /** Sets up the highlighters, and cache's for recalls */
 export const highlightersFromSettings = async (settings: UserConfigSettings) => {
   const themes = settings.themes || (settings.theme ? [settings.theme] : ["light-plus"])
+  if (highlighterCache.get(settings)) return highlighterCache.get(settings)!
 
-  return await Promise.all(
+  const highlighters = await Promise.all(
     themes.map(async theme => {
       // You can put a string, a path, or the JSON theme obj
       const themeName = (theme as any).name || theme
-      const cached = highlighterCache.get(themeName)
-      if (cached) {
-        return cached
-      }
-
       const highlighter = await getHighlighter({ ...settings, theme, themes: undefined })
 
       // @ts-ignore - https://github.com/shikijs/shiki/pull/162 will fix this
       highlighter.customName = themeName
-      highlighterCache.set(themeName, highlighter)
       return highlighter
     })
   )
+
+  highlighterCache.set(settings, highlighters)
+  return highlighters
 }
 
 const amendSettingsForDefaults = (settings: UserConfigSettings) => {
