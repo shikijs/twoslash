04f7989a9ab54661c1690f1de38f405989cba3fc
Orta <git@orta.io>
2021-06-26 14:56:33 +0100

    Fix the code sample on the site

--- site/examples/theme.ts
+++ site/examples/theme.ts
@@ -1,11 +1,11 @@
 // twoslash: { themes: ["../../../script/shiki-twoslash", 'dark-plus',  'github-dark',  'github-light',  'light-plus',  'material-theme-darker',  'material-theme-default', 'material-theme-lighter',  'min-light',  'min-dark',  'monokai', 'slack-theme-ochin', 'solarized-light' , 'nord',  'slack-theme-dark-mode',    'material-theme-ocean',  'solarized-dark',  'material-theme-palenight' ] }
 // @errors: 2540
 interface Todo {
-    title: string;
-  }
-  
-  const todo: Readonly<Todo> = {
-    title: "Delete inactive users",
-  };
-  
-  todo.title = "Hello";
+  title: string;
+}
+
+const todo: Readonly<Todo> = {
+  title: "Delete inactive users",
+};
+
+todo.title = "Hello";
