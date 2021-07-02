9c0ed2297cecad15796847b927ffe30dec949370
Frenco <hey@frenco.dev>
2021-07-02 22:41:59 +0630

    feat: add actual copy functionality

--- packages/docusaurus-preset-shiki-twoslash/docusaurus-theme-shiki-twoslash/theme/CodeBlock/index.js
+++ packages/docusaurus-preset-shiki-twoslash/docusaurus-theme-shiki-twoslash/theme/CodeBlock/index.js
@@ -1,26 +1,27 @@
 // Based on https://github.com/facebook/docusaurus/blob/ed9d2a26f5a7b8096804ae1b3a4fffc504f8f90d/packages/docusaurus-theme-classic/src/theme/CodeBlock/index.tsx
 // which is under MIT License as per the banner
 
+import copy from "copy-text-to-clipboard"
 import React, { useRef, useState } from "react"
 import styles from "./styles.module.css"
 import Translate, { translate } from "@docusaurus/Translate"
 
 const CodeBlock = ({ children, className, ...props }) => {
-  const button = useRef(null)
+  const pre = useRef(null)
   const [showCopied, setShowCopied] = useState(false)
 
   const handleCopyCode = () => {
-    console.log("copied woo")
-
+    if (pre.current) {
+      copy(pre.current.querySelector("code").textContent)
+    }
     setShowCopied(true)
     setTimeout(() => setShowCopied(false), 2000)
   }
 
   return (
-    <pre {...props} className={[className, styles.codeBlock].join(" ")}>
+    <pre {...props} className={[className, styles.codeBlock].join(" ")} ref={pre}>
       {children}
       <button
-        ref={button}
         type="button"
         aria-label={translate({
           id: "theme.CodeBlock.copyButtonAriaLabel",
