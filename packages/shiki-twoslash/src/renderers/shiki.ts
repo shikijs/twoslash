42e7724cff4edd1d3a5f7ccff355cf7966176038
Frenco <hey@frenco.dev>
2021-07-03 15:57:09 +0630

    fix: enable line highlighting for non-twoslash codes
    Closes #50

--- packages/shiki-twoslash/src/renderers/shiki.ts
+++ packages/shiki-twoslash/src/renderers/shiki.ts
@@ -1,10 +1,14 @@
 import { escapeHtml } from "../utils"
+import { shouldBeHighlightable, shouldHighlightLine } from "../parseCodeFenceInfo"
 import { HtmlRendererOptions, preOpenerFromRenderingOptsWithExtras } from "./plain"
 
 type Lines = import("shiki").IThemedToken[][]
 
 export function defaultShikiRenderer(lines: Lines, options: HtmlRendererOptions, codefenceMeta: any) {
   let html = ""
+
+  const hasHighlight = shouldBeHighlightable(codefenceMeta)
+  const hl = shouldHighlightLine(codefenceMeta)
 
   html += preOpenerFromRenderingOptsWithExtras(options, codefenceMeta, [])
   if (codefenceMeta.title) {
@@ -17,11 +21,14 @@ export function defaultShikiRenderer(lines: Lines, options: HtmlRendererOptions,
 
   html += `<div class='code-container'><code>`
 
-  lines.forEach(l => {
+  lines.forEach((l, i) => {
     if (l.length === 0) {
       html += `<div class='line'></div>`
     } else {
-      html += `<div class='line'>`
+      const hiClass = hasHighlight ? (hl(i) ? " highlight" : " dim") : ""
+      const prefix = `<div class='line${hiClass}'>`
+      html += prefix
+
       l.forEach(token => {
         html += `<span style="color: ${token.color}">${escapeHtml(token.content)}</span>`
       })
