30e3c2df779ec8d30641d121378def9943544bdd
Orta <git@orta.io>
2021-08-05 22:02:53 +0100

    Uses JSON syntax for annotations

--- packages/shiki-twoslash/src/annotations.ts
+++ packages/shiki-twoslash/src/annotations.ts
@@ -1,23 +1,36 @@
-import { TwoSlashReturn } from "@typescript/twoslash"
+import { TwoslashError, TwoSlashReturn } from "@typescript/twoslash"
 
 export const htmlForTags = (tags: TwoSlashReturn["tags"]) => {
   let html = ""
   tags.forEach(t => {
     if (t.name === "annotate" && t.annotation) {
-      console.log(t.annotation)
       const meta = t.annotation.split(" - ")
       const text = meta.pop()
       const info = (meta[0] || "").trim()
       const flipped = info.includes("right")
-      const arrowRotation = info.split(" ")[1] || (flipped ? "0" : "56")
-      const textRotation = info.split(" ")[2] || "0"
-
-      const arrowSVG = arrow({ flipped, rotation: arrowRotation })
+      let settings = {
+        flipped,
+        arrowRot: flipped ? "0 0 0" : "56 0 0",
+        textDegree: "0deg",
+        top: `${t.line}em`
+      }
+      
+      
+      if (info.includes("{")) {
+        const theInfo =  "{" + info.split("{")[0]
+        try {
+          settings = JSON.parse(theInfo)
+        } catch (error: any) {
+          throw new TwoslashError("Could not parse annotation", `The annotation ${JSON.stringify(t)} could convert '${theInfo}' into JSON`, `Look at ${error.message}.`)
+        }
+      }
+      
+      const arrowSVG = arrow(settings)
 
       html += `
-<div class='twoslash-annotation ${flipped ? "right" : "left"}' style="top: ${t.line}em;">
+<div class='twoslash-annotation ${flipped ? "right" : "left"}' style="top: ${settings.top}">
   ${arrowSVG}
-  <p class='twoslash-annotation-text' style="transform: rotate(${textRotation}deg)">${text}</p>
+  <p class='twoslash-annotation-text' style="transform: rotate(${settings.textDegree})">${text}</p>
 </div>`
     }
   })
@@ -25,14 +38,12 @@ export const htmlForTags = (tags: TwoSlashReturn["tags"]) => {
   return html
 }
 
-const arrow = (style: { flipped: boolean; rotation: string }) => {
+const arrow = (style: { flipped: boolean; arrowRot: string, textDegree: string, top: string }) => {
   const leftInner = `M27 39C26.5 32.7511 21.9 17.5173 7.5 6.57333M16.5 4.04L0.999999 0.999998C3.16667 4.88444 7.5 13.16 7.5 15.1867`
   const rightInner = `M1 39C1.5 32.7511 6.1 17.5173 20.5 6.57333M11.5 4.04L27 0.999998C24.8333 4.88444 20.5 13.16 20.5 15.1867`
   const inner = style.flipped ? leftInner : rightInner
-
-  const rotatePoint = style.flipped ? "0 0" : "20 20"
   
-  return `<svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg" transform="rotate(${style.rotation} ${rotatePoint})">
+  return `<svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg" transform="rotate(${style.arrowRot})">
     <path d="${inner}" stroke="black" />
 </svg>`
 }
